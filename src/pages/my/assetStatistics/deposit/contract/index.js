11d6f8e4bbca33bc9471c1847f0c77942dde2033
ran <ranyun@bao.cn>
2017-04-27 16:29:40 +0800

    ssss

--- src/pages/my/assetStatistics/deposit/contract/index.js
+++ src/pages/my/assetStatistics/deposit/contract/index.js
@@ -3,10 +3,18 @@
  */
 import React,{Component} from 'react'
 import NavBar from '../../../../../components/NavBar'
-
+import wrap from '../../../../../utils/pageWrapper'
+import style from './index.less'
 class index extends Component{
     render(){
-        return(<div>fsafas</div>)
+        return(<div>
+            <NavBar>
+                定存宝B计划合同
+            </NavBar>
+            <iframe className={style.ifr}>
+
+            </iframe>
+        </div>)
     }
 }
-export default index
+export default wrap(index)
