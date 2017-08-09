67c9b378e6b3c8bd580bc928915ed9967e7b2ba2
向国 <xiangguo@bao.cn>
2017-08-09 15:02:15 +0800

    编写修改交易密码页面

--- src/pages/my/setting/tradePasswordModify/index.js
+++ src/pages/my/setting/tradePasswordModify/index.js
@@ -10,6 +10,7 @@ import Alert from '../../../../components/Dialog/alert'
 import ValidateForm  from '../../../../components/BaseValidateForm'
 import Tipbar from '../../../../components/Tipbar'
 import commonStyles from '../../../../css/common.styl'
+import styles from './index.less'
 import util from '../../../../utils/utils.js'
 import {TRADE_PASSWORD_MODIFY} from '../../../../actions/actionTypes'
 import Page from '../../../../components/Page'
@@ -24,15 +25,13 @@ class TradePasswordModify extends React.Component {
     }
   }
   componentDidMount(){
-   const {
-       data,
-       load
-   }=this.props;
-    if (!data){
-       load();
-    }else if (!data.data.isSetTradePassword){
-       this.alert()
-    }
+    passGuard1.generate("kb1",kb,0);
+    passGuard2.generate("kb2",kb,0);
+    $(function(){
+      setTimeout(function(){
+        kb.generate();
+      },100);
+    })
   }
   componentWillReceiveProps({data,resCode}) {
     if (data){
@@ -60,93 +59,31 @@ class TradePasswordModify extends React.Component {
       })
     }
   }
-  alert(){
-    const alertn=this.refs.alert;
-    alertn.show({
-      title: '您还未设置交易密码',
-      okText: '去设置',
-      okCallback:()=>{this.props.push('/user/setting/tradePasswordSet')}
-    })
-  }
-  loadDom(){
-    return(<Loading/>)
-  }
-  loadEnd(){
-    const {
-        pending
-    }=this.props;
-    return(<div>
-      <ValidateForm
-          className={commonStyles.mt15}
-          ref='form'
-          onValid={this.onValid}
-          onInvalid={this.onInvalid}>
-        <BasePasswordInput
-            ref='password'
-            name='password'
-            label='原密码'
-            maxLength={16}
-            placeholder='请输入原交易密码'
-            type='validateItem'
-            reg={{ reg: {reg: util.checkPassword, message: '请输入正确密码'} }}
-            borderType='two' />
-        <BasePasswordInput
-            ref='newPassword'
-            name='newPassword'
-            maxLength={16}
-            label='新密码'
-            placeholder='请输入新交易密码'
-            type='validateItem'
-            reg={{ reg: {reg: util.checkPassword, message: '请输入正确密码'},
-              equalTo: {name: 'password', message: '新密码不能和原密码相同'} }}
-            borderType='three' />
-        <Button
-            className={commonStyles.buttonWrap}
-            text={pending&&<InlineLoading color="rgba(255,255,255,.8)" text={'提交中'} className={commonStyles.loading}/>||'确认提交'}
-            type='submit' />
-      </ValidateForm>
-      { this.state.errorMessage && <Tipbar text={this.state.errorMessage} /> }
-    </div>)
-  }
-  onValid = () => {
-    if (this.props.pending) return
-    const {
-      password: oldPwd,
-      newPassword: password,
-    } = this.refs.form.getValue()
-    this.props.modify({oldPwd, password})
-  }
-
-  onInvalid = (name, value, message) => {
-    this.setState({errorMessage: message})
-  }
-
   openTipbar(message) {
     //this.refs.tipbar.open(message)
   }
 
   render() {
-    const {
-        data,
-        load
-    }=this.props;
-    let Dom;
-    if (data){
-       if (data.data.isSetTradePassword){
-         Dom=this.loadEnd()
-       }
-    }else {
-      Dom=this.loadDom()
-    }
     return (
       <Page>
         <div className={commonStyles.panel}>
           <NavBar onLeft={this.props.pop} title='修改交易密码' />
-          {
-            Dom
-          }
+          <div className={styles.input_group}>
+          <div className={styles.inputs}>
+            <span>原密码</span>
+          <input id="kb1" type="text"/>
+          </div>
+          <div className={styles.inputs}>
+            <span>新密码</span>
+          <input id="kb2" type="text"/>
+          </div>
+          </div>
+          <Button className={styles.submit} text="确认修改" >
+
+          </Button>
           <Alert ref="alert" />
         </div>
+        <Tipbar text={this.state.errorMessage} />
       </Page>
     )
   }
