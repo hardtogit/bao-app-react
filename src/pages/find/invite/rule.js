dcd3c150b3f62a6880120178ce178586f002fd3e
向国 <xiangguo@bao.cn>
2017-10-26 17:28:46 +0800

    修复iosiframe滚动

--- src/pages/find/invite/rule.js
+++ src/pages/find/invite/rule.js
@@ -20,8 +20,10 @@ class Index extends React.Component{
             <div>
                 <NavBar backgroundColor="#7f48cb" onLeft={this.props.pop}>活动详情</NavBar>
             <div className={styles.bg}>
+                <div className={styles.iframe}>
                 <iframe src="https://www.bao.cn/special/inviteFriends/wap/index.html"   style={{width:'100%',height:'100%',position:'absolute',top:'44px',border:'none',bottom:'44px'}}></iframe>
-                <div onClick={()=>{this.props.replace('/find/inviteFriends')}}  style={{position:'fixed',lineHeight:'44px',fontSize:'18px',bottom:'0',height:'44px',backgroundColor:'#fed74e',width:'100%',color:'#30194f',textAlign:'center'}}>
+                </div>
+                    <div onClick={()=>{this.props.replace('/find/inviteFriends')}}  style={{position:'fixed',lineHeight:'44px',fontSize:'18px',bottom:'0',height:'44px',backgroundColor:'#fed74e',width:'100%',color:'#30194f',textAlign:'center'}}>
                   马上邀请</div>
             </div>
             </div>
