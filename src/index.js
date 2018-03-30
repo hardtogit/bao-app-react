ee162ca1c85d054b72cc90239fea9125b6c8e0e2
向国 <xiangguo@bao.cn>
2018-03-30 09:39:06 +0800

    按需加载样式调整

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
+//坑人键盘专用
+require('!file-loader?name=static/js/[name].[ext]!./assets/lib/jquery.js');
 ReactDOM.render(
 	<App />,
 	document.getElementById('root')
