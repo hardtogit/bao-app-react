394188869c08c5801cd3575d3cdf201101af0e12
向国 <xiangguo@bao.cn>
2018-04-13 16:57:44 +0800

    修复状态验证

--- src/components/Permission/index.js
+++ src/components/Permission/index.js
@@ -4,34 +4,40 @@
  * email:413401168@qq.com.
  * use:auto...
  */
-import {push} from "react-router-redux"
-export const getAuthDetail=()=>{
-    let storeData=JSON.parse(sessionStorage.getItem('bao-store'));
-        if(storeData&&storeData.isRegister&&storeData.authInstrCd.length!=0){
-            return 1 //表示注册并开通
-        }else{
-            if(storeData&&storeData.isRegister){
-                return 2//表示没有免密授权
-            }else{
-                return 3//表示既没有注册存管也没有免密授权
-            }
-        }
+export const getAuthDetail=()=> {
+     let storeData = JSON.parse(sessionStorage.getItem('bao-store'));
+     if(storeData&&storeData.isRegister){//表示开通存管
+         if(storeData.isBindBankcard) {//老用户开通存管没绑卡判断
+              if(storeData.authInstrCd.length!=0){
+                  return 1 //表示一切正常
+              }else{
+                  return 2 //表示没有免密授权
+              }
+         }else{
+             return 3 //表示没有绑定银行卡
+         }
+         }else{
+         return 3
+     }
 },
     platFormGetAuthDetail=()=>{
         let storeData=JSON.parse(sessionStorage.getItem('bao-store'));
        if(storeData&&storeData.isAuthIdentity&&storeData.isSecurityCard){
            return 1 //表示老用户实名认证，并且绑定了安全卡
        }else{
-           if(storeData&&storeData.isRegister&&storeData.authInstrCd.length!=0){
-               return 1 //表示注册并开通
-           }else{
-               if(storeData&&storeData.isRegister){
-                   return 2//表示没有免密授权
+           if(storeData&&storeData.isRegister){//表示开通存管
+               if(storeData.isBindBankcard) {//老用户开通存管没绑卡判断
+                   if(storeData.authInstrCd.length!=0){
+                       return 1 //表示一切正常
+                   }else{
+                       return 2 //表示没有免密授权
+                   }
                }else{
                    return 3//表示既没有注册存管也没有免密授权
                }
+           }else{
+               return 3
            }
        }
-
     }
 
